eee105149d342294c4aabbf53d6131fdd51c44d1
Brandon Braner <brandon.braner@gmail.com>
2021-10-05 23:26:01 -0500

    fix cors middleware

--- app/cors.py
+++ app/cors.py
@@ -2,14 +2,5 @@ from typing import List
 
 
 def get_cors_domains(env: str) -> List:
-    '''Return a list of domains we accept for cors'''
-    if env == 'local':
-        return [
-            'http://localhost:3000',
-            'http://localhost:8000',
-            'http://app.released.local',
-            'http://api.released.local'
-        ]
-
-    if env == 'PROD':
-        return['*']
+    """Return a list of domains we accept for cors"""
+    return ['*']
